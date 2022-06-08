3cb9122334d9abdd95f79b172a58c85a88ac6847
Chandrakanth9394 <95459551+Chandrakanth9394@users.noreply.github.com>
2022-06-08 11:36:54 +0530

    26-05-2022

---
+++ course_outcome_4/Q1/Graphics/sqaure.java
@@ -0,0 +1,12 @@
+package Graphics;
+interface sqa
+   {
+	 public void area(int s);
+   }
+public class Sqaure 
+  {
+    public void area(int s)
+       {
+    	 System.out.println("area of square:"+s*s);
+       }
+  }
